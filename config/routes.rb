0109b93cb01d6c1c1b592449b1f8e08e790216a9
kota-aso <kotaaso1212@gmail.com>
2022-10-19 15:51:48 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'usersrails/g'
-  get 'usersrails/model'
-  get 'usersrails/room'
   devise_for :users
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
